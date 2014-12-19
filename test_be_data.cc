02deb3cb88e765004bbeb346697e8c07842fa01d
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2014-12-19 10:27:55 +0000

    more readable output from test_be_data
    git-svn-id: file:///usr/local/svn/cpplib/trunk/ocl@943 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test_be_data.cc
+++ test_be_data.cc
@@ -2,29 +2,33 @@
 
 int main()
 {
-        int r;
-        try {
-                std::vector<cl::Device> v(ocl::impl::devices());
-                std::cout << "count of devices: " << v.size() << std::endl;
-                for (std::size_t i = 0; i< v.size(); ++i) {
-                        std::cout << ocl::impl::device_info(v[i]);
-                }
-                ocl::impl::device dd(ocl::impl::default_device());
-                std::cout << "selected device: \n";
-                std::cout << ocl::impl::device_info(dd);
-                
-                const ocl::impl::device& bed =
-                        ocl::impl::be_data::instance()->d();
-                std::cout << "\nselected backend device: \n";
-                std::cout << ocl::impl::device_info(bed);
-                r = 0;
+    int r;
+    try {
+        std::vector<cl::Device> v(ocl::impl::devices());
+        std::cout << std::string(60, '-') << std::endl;
+        std::cout << "count of devices: " << v.size() << std::endl;
+        for (std::size_t i = 0; i< v.size(); ++i) {
+            std::cout << std::string(60, '-') << std::endl;
+            std::cout << ocl::impl::device_info(v[i]);
         }
-        catch (const ocl::impl::error& e) {
-                std::cout << "caught exception: " << e.what()
-                          << '\n'
-                          << ocl::impl::err2str(e)
-                          << std::endl;
-                r = 3;
-        }
-        return r;
+        ocl::impl::device dd(ocl::impl::default_device());
+        std::cout << std::string(60, '-') << std::endl;
+        std::cout << "selected device: \n";
+        std::cout << ocl::impl::device_info(dd);
+
+        const ocl::impl::device& bed =
+            ocl::impl::be_data::instance()->d();
+        std::cout << std::string(60, '-') << std::endl;
+        std::cout << "selected backend device: \n";
+        std::cout << ocl::impl::device_info(bed);
+        r = 0;
+    }
+    catch (const ocl::impl::error& e) {
+        std::cout << "caught exception: " << e.what()
+                  << '\n'
+                  << ocl::impl::err2str(e)
+                  << std::endl;
+        r = 3;
+    }
+    return r;
 }
