9d75fe55ceb2f868ae332aeae812c24f9cb7d6ef
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2020-01-05 13:26:20 +0000

    better generator
    git-svn-id: file:///usr/local/svn/cpplib/trunk/ocl@4182 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test/test_math.cc
+++ test/test_math.cc
@@ -100,6 +100,21 @@ namespace cftal {
 
 namespace ocl {
 
+    namespace impl {
+        __cf_body
+        gen_horner(const std::string& tname,
+                   const std::string& coeff_tname,
+                   size_t n);
+        __cf_body
+        gen_horner2(const std::string& tname,
+                    const std::string& coeff_tname,
+                    size_t n);
+        __cf_body
+        gen_horner4(const std::string& tname,
+                    const std::string& coeff_tname,
+                    size_t _N);
+    }
+    
     template <typename _X, typename _C1, typename _C0>
     auto
     horner(const _X& x, const _C1& c1, const _C0& c0);
@@ -155,28 +170,125 @@ namespace ocl {
     }
 };
 
+ocl::impl::__cf_body
+ocl::impl::
+gen_horner(const std::string& tname,
+           const std::string& cname,
+           size_t n)
+{
+    std::ostringstream s;
+    s << "horner_" << n;
+    const std::string hname=s.str();
+    s.str("");
+    s << tname << " " << hname << "( "
+      << tname << " x, "
+      << " __arg_local const " << cname << "* c)\n"
+        "{\n"
+        "    "<< tname << " r=c[0];\n";
+    for (size_t i=1; i<n; ++i) {
+        s << "    r=x*r+c["<< i<<"];\n";
+    }
+    s << "    return r;\n"
+         "}\n";
+    return __cf_body(hname, s.str());
+}
+
+ocl::impl::__cf_body
+ocl::impl::
+gen_horner2(const std::string& tname,
+            const std::string& cname,
+            size_t n)
+{
+    std::ostringstream s;
+    s << "horner2_" << n;
+    const std::string hname=s.str();
+    s.str("");
+    s.str("");
+    s << tname << " " << hname << "( "
+      << tname << " x, "
+      << tname << " x2, "
+      << " __arg_local const " << cname << "* c)\n"
+        "{\n"
+        "    " << tname << " r0=c[0];\n"
+        "    " << tname << " r1=c[1];\n";
+    const std::size_t _NE= n & ~(std::size_t(1));
+    for (size_t i=2; i<_NE; i+=2) {
+        s << "    r0=x2*r0+c["<< i<<"];\n";
+        s << "    r1=x2*r1+c["<< i+1<<"];\n";
+    }
+    s << "    " << tname << " r= x*r0+r1\n";
+    if ( n & 1) {
+        s << "    r = x*r + c[" << n-1 << "]\n";
+    }
+    s << "    return r;\n"
+         "}\n";
+    return __cf_body(hname, s.str());
+}
+
+ocl::impl::__cf_body
+ocl::impl::
+gen_horner4(const std::string& tname,
+            const std::string& cname,
+            size_t n)
+{
+    std::ostringstream s;
+    s << "horner4_" << n;
+    const std::string hname=s.str();
+    s.str("");
+    s.str("");
+    s << tname << " " << hname << "( "
+      << tname << " x, "
+      << tname << " x2, "
+      << tname << " x4, "
+      << " __arg_local const " << cname << "* c)\n"
+        "{\n"
+        "    " << tname << " r0=c[0];\n"
+        "    " << tname << " r1=c[1];\n"
+        "    " << tname << " r2=c[2];\n"
+        "    " << tname << " r3=c[3];\n";
+    const std::size_t _NE= n & ~(std::size_t(3));
+    for (size_t i=4; i<_NE; i+=4) {
+        s << "    r0=x4*r0+c["<< i<<"];\n";
+        s << "    r1=x4*r1+c["<< i+1<<"];\n";
+        s << "    r2=x4*r2+c["<< i+2<<"];\n";
+        s << "    r3=x4*r3+c["<< i+3<<"];\n";
+    }
+    s << '\n';
+    s << "    " << tname << " r02 = x2*r0 + r2;\n";
+    s << "    " << tname << " r13 = x2*r1 + r3;\n";
+    s << "    " << tname << " r = x*r02 + r13;\n";
+    const std::size_t _NR= n & std::size_t(3);
+    switch (_NR) {
+    default:
+        break;
+    case 1:
+        s << "    r= x*r + c[" << n-1 <<"];\n";
+        break;
+    case 2:
+        s << "    " << tname << " a= x2*r + c["<< n-1 << "];\n";
+        s << "    r = x*c[" << n-2 << "] * a;\n";
+        break;
+    case 3:
+        s << "    " << tname << " a= x2*r + c["<< n-2 << "];\n";
+        s << "    " << tname
+          << " b= x2*c["<< n-3 << "] + c["<< n - 1 << "];\n";
+        s << "    r = x*a +b\n;";
+        break;
+    }
+    s << "    return r;\n"
+         "}\n";
+    return __cf_body(hname, s.str());
+}
+
 template <typename _T, typename _C, size_t _N>
 auto
 ocl::horner(const dvec<_T>& x, const _C(&a)[_N])
 {
 #if 1
-    std::ostringstream hs;
-    hs << "horner" << _N; 
-    const std::string hname=hs.str();
-    hs.str("");
     const std::string tname= be::type_2_name<_T>::v();
     const std::string cname= be::type_2_name<_C>::v();
-    hs << tname << " " << hname << "( " << tname
-       << " v0, __arg_local const " << cname << "* c)\n"
-        "{\n"
-        "    "<< tname << " r=c[0];\n";
-    for (size_t i=1; i<_N; ++i) {
-        hs << "    r=v0*r+c["<< i<<"];\n";
-    }
-    hs << "    return r;\n"
-          "}\n";
-    const std::string hbody=hs.str();
-    return custom_func<_T>(hname, hbody, x, a); 
+    auto hb=impl::gen_horner(tname, cname, _N);
+    return custom_func<_T>(hb.name(), hb.body(), x, a); 
 #else
     static_assert(_N > 0, "invalid call to horner(x, array)");
     const _C* pa=a;
