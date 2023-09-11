44abe2b5931ab0049d72c8e15dda3f875f6aef94
axze <az0123456@gmx.de>
2023-09-11 17:35:43 +0200

    backend devices without out of order queues
    allow the selection of the queue flags from the device queue properties

--- src/be_dev_ctx_queue.cc
+++ src/be_dev_ctx_queue.cc
@@ -3,11 +3,26 @@
 #include <iomanip>
 #include <thread>
 
+namespace {
+
+    cl_command_queue_properties
+    enable_ooo_execution(const ocl::cl::device& d) {
+        cl_command_queue_properties p=0;
+        cl_command_queue_properties cqp(
+            d.get_info<cl_command_queue_properties>(CL_DEVICE_QUEUE_PROPERTIES));
+        if ((cqp & CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE)==
+            CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE) {
+            p |= ocl::cl::queue::enable_out_of_order_execution;
+        }
+        return p;
+    }
+}
+
 ocl::be::dev_ctx_queue::
 dev_ctx_queue()
     : _d(default_device()),
       _c(_d),
-      _q(_c, _d, queue::enable_out_of_order_execution),
+      _q(_c, _d, enable_ooo_execution(_d)),
       _wl(), _mtx()
 {
 }
@@ -16,7 +31,7 @@ ocl::be::dev_ctx_queue::
 dev_ctx_queue(const device& dd)
     : _d(dd),
       _c(_d),
-      _q(_c, _d, queue::enable_out_of_order_execution),
+      _q(_c, _d, enable_ooo_execution(_d)),
       _wl(), _mtx()
 {
 }
@@ -24,7 +39,7 @@ dev_ctx_queue(const device& dd)
 ocl::be::dev_ctx_queue::
 dev_ctx_queue(const device& dd, const context& ctx)
     : _d(dd), _c(ctx),
-      _q(_c, _d, queue::enable_out_of_order_execution),
+      _q(_c, _d, enable_ooo_execution(_d)),
       _wl(), _mtx()
 {
 }
