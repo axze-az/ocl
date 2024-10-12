a2f8ec52b0110f53bdc2554ad2f89072fbb71fd7
axdev <az0123456@gmx.de>
2024-10-12 16:15:27 +0200

    read f32_div_sqrt_correctly_round from the device

--- src/expr_kernel.cc
+++ src/expr_kernel.cc
@@ -155,7 +155,7 @@ insert_headers(std::ostream& s,
     s << "#if defined (cl_khr_fp16)\n"
          "#pragma OPENCL EXTENSION cl_khr_fp16 : enable\n"
          "#endif\n\n";
-#if 1
+#if 0
     const int f32_div_sqrt_correctly_round=0;
 #else
     const auto& d=b->dcq().d();
