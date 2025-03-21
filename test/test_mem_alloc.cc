54d15793abb06e0d89abc007aa3ce6408c41317d
axdev <az0123456@gmx.de>
2025-03-21 17:23:54 +0100

    add a memory allocation test

---
+++ test/test_mem_alloc.cc
@@ -0,0 +1,52 @@
+#include <cftal/vec.h>
+#include <ocl/ocl.h>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <map>
+#include <atomic>
+#include <mutex>
+#include <memory> // for shared_ptr
+#include <cmath>
+
+namespace ocl {
+}
+
+
+int main()
+{
+    try {
+        using namespace cftal;
+        using namespace ocl;
+
+        using ftype = float;
+
+        constexpr const std::size_t elem_count=(64*1024*1024ULL)/sizeof(ftype);
+        std::vector<dvec<ftype> > vv;
+
+        constexpr const std::size_t max_count=
+            (8*1024*1024ULL*1024ULL)/sizeof(ftype)/elem_count;
+        std::cout << std::fixed << std::setprecision(2);
+        for (std::size_t i=0; i<max_count; ++i) {
+            double mb=(double(vv.size())*elem_count*sizeof(ftype))/
+                double(1024*1024);
+            std::cout << "Total allocated memory: " << mb << " MB\n";
+            auto init_val=static_cast<ftype>(i+1);
+            dvec<ftype> vi(init_val, elem_count);
+            vv.emplace_back(std::move(vi));
+            vv.back() += init_val;
+        }
+        std::cout << "test passed\n";
+    }
+    catch (const ocl::be::error& e) {
+        std::cout << "caught exception: ocl::be::error " << e.what()
+                  << '\n'
+                  << e.error_string()
+                  << std::endl;
+    }
+    catch (const std::runtime_error& e) {
+        std::cout << "caught exception: runtime error: " << e.what()
+                  << std::endl;
+    }
+    return 0;
+}
